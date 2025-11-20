468a6470158f3f6c596b18321d6e0b97979ae264
Itaysc23 <itay.schwartz@post.runi.ac.il>
2025-11-20 14:13:07 +0200

    finalll

--- Anagram.java
+++ Anagram.java
@@ -30,63 +30,52 @@ public class Anagram {
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
     String processedStr1 = preProcess(str1);
-    String processedStr2 = preProcess(str2);
-    
-    if (processedStr1.length() != processedStr2.length()) {
-        return false;
-    }
-    
-    StringBuilder tempStr2 = new StringBuilder(processedStr2);
-    
-    for (int i = 0; i < processedStr1.length(); i++) {
-        char charToFind = processedStr1.charAt(i);
-        
-        int indexInStr2 = -1;
-        for (int j = 0; j < tempStr2.length(); j++) {
-            if (tempStr2.charAt(j) == charToFind) {
-                indexInStr2 = j;
-                break; 
-            }
-        }
-        
-        if (indexInStr2 == -1) {
-            return false;
-        }
-        
-        tempStr2.deleteCharAt(indexInStr2);
-    }
-    
-    return true;
-}	   
+		String processedStr2 = preProcess(str2);
+		String tempStr2 = processedStr2;
+		if (processedStr1.length() != processedStr2.length()) {
+			return false;
+		}	else{
+			for (int i = 0; i < processedStr1.length(); i++) {
+				char currentChar = processedStr1.charAt(i);
+				int indexInStr2 = tempStr2.indexOf(currentChar);
+				if ( indexInStr2 == -1) {
+					return false;
+				} else{
+					tempStr2 = tempStr2.substring(0, indexInStr2) + tempStr2.substring(indexInStr2+1);
+				}
+			}
+		}
+
+		return true;
+	}   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		StringBuilder result = new StringBuilder();
-    for (int i = 0; i < str.length(); i++) {
-        char ch = str.charAt(i);
-        if (Character.isLetter(ch)) {
-            result.append(Character.toLowerCase(ch));
-        } 
-    } 
-    return result.toString(); 
-} 
+		String lowercaseStr = str.toLowerCase();
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		String processedString = "";
+		for (int i = 0; i < lowercaseStr.length(); i++) {
+			char currentChar = lowercaseStr.charAt(i);
+			if (alphabet.indexOf(currentChar) != -1) {
+				processedString = processedString + currentChar;
+			}
+		}
+		return processedString;
+	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		StringBuilder original = new StringBuilder(str);
-    StringBuilder randomResult = new StringBuilder();
-    
-    while (original.length() > 0) {
-        int randomIndex = (int) (Math.random() * original.length());
-        
-        char randomChar = original.charAt(randomIndex);
-        randomResult.append(randomChar);
-        
-        original.deleteCharAt(randomIndex);
-    }
-    
-    return randomResult.toString();
-}
+		String source = str;
+		String result = "";
+
+		while (source.length() > 0) {
+			int randomIndex = (int)(Math.random() * source.length());
+			char randomChar = source.charAt(randomIndex);
+			result = result + randomChar;
+			source = source.substring(0, randomIndex) + source.substring(randomIndex + 1);
+		}
+			return result;
+	}
 }
